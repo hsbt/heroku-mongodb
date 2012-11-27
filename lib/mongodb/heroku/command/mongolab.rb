803c565190fbcc80b9af1ce2b94f54abf47d37d8
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2012-11-28 03:21:04 +0900

    Update to use heroku-api via Heroku::Auth.api

--- lib/mongodb/heroku/command/mongolab.rb
+++ lib/mongodb/heroku/command/mongolab.rb
@@ -2,7 +2,7 @@ require 'uri'
 
 class Heroku::Command::Mongolab < Heroku::Command::Base
   def dump
-    uri = heroku.mongolab_vars(app)
+    uri = config_vars
     if uri && args[0]
       config = URI(uri)
       system("mongodump -h #{config.host} --port #{config.port} -u #{config.user} -p #{config.password} -d #{config.path.sub(/\A\//, '')} -o #{args[0]}")
@@ -10,10 +10,21 @@ class Heroku::Command::Mongolab < Heroku::Command::Base
   end
 
   def restore
-    uri = heroku.mongolab_vars(app)
+    uri = config_vars
     if uri && args[0]
       config = URI(uri)
       system("mongorestore -h #{config.host} --port #{config.port} -u #{config.user} -p #{config.password} -d #{config.path.sub(/\A\//, '')} #{args[0]}")
+    end
+  end
+
+  private
+
+  def config_vars
+    config = api.get_config_vars(app).body
+    if config.has_key?('MONGOLAB_URI')
+      config['MONGOLAB_URI']
+    else
+      nil
     end
   end
 end
